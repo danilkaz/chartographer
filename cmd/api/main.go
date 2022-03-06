6a661902d4b06905381ade562f065aad05517ac8
danilkaz <cmaster057@gmail.com>
2022-03-06 16:04:09 +0500

    add env parsing and database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,49 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/danilkaz/chartographer/internal/models"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
 
+func initDatabaseConfig() models.DatabaseConfig {
+	return models.DatabaseConfig{
+		Host:     os.Getenv("POSTGRES_HOST"),
+		Port:     os.Getenv("POSTGRES_PORT"),
+		Username: os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		Database: os.Getenv("POSTGRES_DB"),
+	}
+}
+
+func openDatabaseConnection(config models.DatabaseConfig) (*gorm.DB, error) {
+	connectionString := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s",
+		config.Host,
+		config.Port,
+		config.Username,
+		config.Password,
+		config.Database,
+	)
+	connection, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return connection, nil
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("No .env file found")
+	}
+	databaseConfig := initDatabaseConfig()
+	databaseConnection, err := openDatabaseConnection(databaseConfig)
+	if err != nil {
+		log.Fatal("Database connection error")
+	}
+	fmt.Println(databaseConnection)
 }
---
+++ internal/models/databaseConfig.go
@@ -0,0 +1,9 @@
+package models
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	Database string
+}
