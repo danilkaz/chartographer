d6fddb09e3f258f526908d2577c4e984bca1e802
danilkaz <cmaster057@gmail.com>
2022-03-09 15:48:33 +0500

    add change part of image method

---
+++ internal/service/bitmapImage.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"image"
+	"image/color"
+)
+
+type BitmapImage struct {
+	image.Image
+	colors map[image.Point]color.Color
+}
+
+func NewBitmapImage(bitmapImage image.Image) *BitmapImage {
+	return &BitmapImage{bitmapImage, map[image.Point]color.Color{}}
+}
+
+func (b *BitmapImage) SetColor(x, y int, color color.Color) {
+	b.colors[image.Point{X: x, Y: y}] = color
+}
+
+func (b *BitmapImage) At(x, y int) color.Color {
+	if c := b.colors[image.Point{X: x, Y: y}]; c != nil {
+		return c
+	}
+	return b.Image.At(x, y)
+}
+
+func (b *BitmapImage) ChangePartOfImage(x, y, width, height int, otherImage BitmapImage) {
+	for row := y; row < y+height; row++ {
+		for column := x; column < x+width; column++ {
+			b.SetColor(row, column, otherImage.At(row, column))
+		}
+	}
+}
