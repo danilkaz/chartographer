38c3cc43910c105df475307a5c2e6c4eb34de228
danilkaz <cmaster057@gmail.com>
2022-03-20 13:49:26 +0500

    extreme cases handling

--- internal/models/charta.go
+++ internal/models/charta.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"github.com/danilkaz/chartographer/internal/models/errors"
 	"github.com/google/uuid"
 	"image"
 	"math"
@@ -26,25 +27,35 @@ func NewChartaWithId(id uuid.UUID, img image.Image) *Charta {
 	}
 }
 
-func (c *Charta) ChangePartOfImage(x, y, width, height int, otherCharta *Charta) {
+func (c *Charta) ChangePartOfImage(x, y, width, height int, otherCharta *Charta) error {
 	bounds := c.Image.Bounds()
 	startRow := int(math.Max(0, float64(y)))
 	endRow := int(math.Min(float64(bounds.Dy()), float64(y+height)))
 	startColumn := int(math.Max(0, float64(x)))
 	endColumn := int(math.Min(float64(bounds.Dx()), float64(x+width)))
+	isChanged := false
 	for row := startRow; row < endRow; row++ {
 		for column := startColumn; column < endColumn; column++ {
 			c.Image.Set(column, row, otherCharta.Image.At(column-startColumn, row-startRow))
+			isChanged = true
 		}
 	}
+	if !isChanged {
+		return errors.NotChangedError{}
+	}
+	return nil
 }
 
-func (c *Charta) SubCharta(x, y, width, height int) image.Image {
+func (c *Charta) GetSubImage(x, y, width, height int) (image.Image, error) {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	bounds := c.Image.Bounds()
+	if x+width <= 0 || y+height <= 0 || x >= bounds.Dx() || y >= bounds.Dy() {
+		return img, errors.OutOfScopeError{}
+	}
 	for row := 0; row < height; row++ {
 		for column := 0; column < width; column++ {
 			img.Set(column, row, c.Image.At(column+x, row+y))
 		}
 	}
-	return img
+	return img, nil
 }
