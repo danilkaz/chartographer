39699756baf25954836a1cdf193bbd1c1505100c
danilkaz <cmaster057@gmail.com>
2022-03-20 12:06:20 +0500

    update main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,25 +1,62 @@
 package main
 
 import (
+	"context"
+	"github.com/danilkaz/chartographer/internal"
 	"github.com/danilkaz/chartographer/internal/repository"
 	"github.com/danilkaz/chartographer/internal/service"
 	"github.com/danilkaz/chartographer/internal/transport/rest"
-	"github.com/google/uuid"
-	"net/http"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+	"time"
 )
 
+func init() {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal("Config does not exist")
+	}
+}
+
 func main() {
-	db := map[uuid.UUID]bool{}
-	r := repository.NewRepository(db)
-	s := service.NewService(r)
-	h := rest.NewHandler(s)
-	err := http.ListenAndServe(":8000", h.InitRoutes())
+	port := viper.GetString("port")
+
+	wd, err := os.Getwd()
 	if err != nil {
 		return
 	}
-	//file, _ := os.Open("image.bmp")
-	//img, _ := bmp.Decode(file)
-	//file2, _ := os.Create("imagenew.bmp")
-	//charta := models.Charta{Image: img}
-	//bmp.Encode(file2, charta.SubCharta(10, 10, 500, 200))
+	path := filepath.Join(wd, filepath.Dir(os.Args[1]))
+
+	if err = os.MkdirAll(path, os.ModePerm); err != nil {
+		log.Fatalf("Unable to create directory %s", path)
+	}
+
+	r := repository.NewRepository(path)
+	s := service.NewService(r)
+	h := rest.NewHandler(s)
+
+	server := internal.NewServer()
+
+	go func() {
+		if err = server.Run(port, h.InitRoutes()); err != nil {
+			log.Fatal("Server run error")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+	<-quit
+
+	ctx, shutdown := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdown()
+
+	if err = server.Stop(ctx); err != nil {
+		log.Fatal("Server shutdown error")
+	}
 }
